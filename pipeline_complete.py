bfc902a09d33ba6a7ec484cfc54e11becdd216c2
Sara Rey <sara.rey.manchester@gmail.com>
2019-06-20 16:53:45 +0100

    Incremental update

--- pipeline_complete.py
+++ pipeline_complete.py
@@ -9,7 +9,7 @@ run_id = "190607_M00766_0229_000000000-CCWJY"
 
 run_path = os.path.join(path_to_data,run_id)
 
-print(run_path)
+#print(run_path)
 
 
 def main():
@@ -22,11 +22,12 @@ def sample_level(directory):
     panels_path_list = [os.path.join(directory, d) for d in panels_list]
     print(panels_path_list)
 
-    panels_samples_list = ([filter(lambda x: os.path.isdir(os.path.join(y, x)), os.listdir(y)) for y in panels_path_list])
-    print([f for f in panels_samples_list])
-    
-    #samples_path_list = [os.path.join(directory, di) for di in samples_list]
-    #print(samples_path_list)
+    for i in panels_path_list:
+        print(i)
+        samples_list = filter(lambda x: os.path.isdir(os.path.join(i, x)), os.listdir(i))
+        samples_path_list = [os.path.join(i, s) for s in samples_list]
+        print(samples_path_list)
+
 
 
     return None
