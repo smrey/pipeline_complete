ae0d53ec5461f1cebd5ceb95552fb0629312c6e7
Sara <sara.rey.manchester@gmail.com>
2019-06-25 21:47:38 +0100

    added nested dictionary

--- pipeline_complete.py
+++ pipeline_complete.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import glob
 
 path_to_data = os.path.join("/","Users","sararey","tmp") # hard code to results directory
 path_to_data = os.path.join("C:\\","Users","saram","Documents","Work","mockdata")
@@ -20,16 +21,37 @@ def main():
 
 def sample_level(directory):
     panels_list = filter(lambda x: os.path.isdir(os.path.join(directory, x)), os.listdir(directory))
-    print(panels_list)
+    #print(panels_list)
     panels_path_list = [os.path.join(directory, d) for d in panels_list]
-    print(panels_path_list)
+    #print(panels_path_list)
 
+    panels_dict = {}
     for i in panels_path_list:
-        print(i)
+        #print(i)
+        panel_name = os.path.split(i)[1]
+        print(panel_name)
         samples_list = filter(lambda x: os.path.isdir(os.path.join(i, x)), os.listdir(i))
         samples_path_list = [os.path.join(i, s) for s in samples_list]
-        print(samples_path_list)
+        #print(samples_path_list)
 
+
+        samples_dict = {}
+        for fl in samples_path_list:
+            #print(fl)
+            samples_name = os.path.split(fl)[1]
+            print(samples_name)
+            values_dict = {}
+            with open(glob.glob(fl + "\*.variables")[0]) as f:
+                for line in f:
+                    splitter = line.rstrip().split("=")
+                    the_key = splitter[0]
+                    the_value = splitter[1]
+                    values_dict[the_key] = the_value
+                print(values_dict)
+            samples_dict[samples_name] = values_dict
+        print(samples_dict)
+        panels_dict[panel_name] = samples_dict
+    print(panels_dict)
 
 
     return None
